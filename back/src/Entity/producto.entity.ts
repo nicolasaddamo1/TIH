9c6d293a5d12d4eee6919b6c3b3864ed5d53c1ed
Nico <nicolasaddamo1@gmail.com>
2024-10-29 19:55:43 -0300

    finished product entity

---
+++ back/src/Entity/producto.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("producto")
+export class Producto {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column({type:"varchar", length:50})
+    nombre: string
+
+    @Column({type:"int"})
+    precio: number
+
+    @Column({type:"int"})
+    stock: number
+
+    @Column({type:"varchar", length:50})
+    imagen: string
+
+    @Column({type:"varchar", length:50})
+    categoria: string
+}
