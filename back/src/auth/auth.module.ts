19bbef32e61a53995b9fc44e88b19220734eec75
Nico <nicolasaddamo1@gmail.com>
2024-10-30 16:43:27 -0300

    added auth

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { Usuario } from 'src/Entity/usuario.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Usuario]), 
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Usuario } from 'src/Entity/usuario.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
+    constructor(
+        @InjectRepository(Usuario) private usuarioRepository: Repository<Usuario>,
+    ){}
     async login (email: string, password: string) {
-        return {email, password}
+
+        const foundedUser = await this.usuarioRepository.findOneBy({email, password})
+        if (!foundedUser) {
+            throw new Error('User not found')
+        }
+        if (foundedUser.password !== password || foundedUser.email !== email) {
+            throw new Error('ivalid credentials')
+        }
+
+        return "Login Success"
         
     }
 }
