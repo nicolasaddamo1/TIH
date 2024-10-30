a218b2a37993ed0e5b1c29c8423a0b0b8623ac05
Nico <nicolasaddamo1@gmail.com>
2024-10-30 10:11:20 -0300

    added auth

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
+import { ProductoModule } from './producto/producto.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [UsuarioModule, ProductoModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
