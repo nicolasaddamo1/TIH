29351561e01882bda2d5f884021ae93e7df4799f
Nico <nicolasaddamo1@gmail.com>
2024-11-15 17:01:41 -0300

    ready login

--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
 
     ){}
     async login (email: string, password: string) {
-
+        console.log(email, password);
         const user = await this.usuarioRepository.findOne({ where: { email }});
         if (!user) throw new NotFoundException('Credenciales invalidas.');
         let userRoles: Role[] = [user.role];
