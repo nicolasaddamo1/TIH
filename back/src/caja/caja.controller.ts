0e85509ad39330c6740ed88ed5bb002808344d15
Nico <nicolasaddamo1@gmail.com>
2024-11-25 16:33:02 -0300

    ready get de caja c/limite

--- back/src/caja/caja.controller.ts
+++ back/src/caja/caja.controller.ts
@@ -1,5 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
+import { CajaService } from './caja.service';
 
 @Controller('caja')
 export class CajaController {
+    constructor(
+        private readonly cajaService: CajaService
+    ) {}
+
+    @Get()
+    async getAllCajas(
+        @Body('limit') limit:number
+    ) {  
+        return this.cajaService.getAllCajas(limit);
+    }
 }
--- back/src/caja/caja.service.ts
+++ back/src/caja/caja.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Caja } from 'src/Entity/caja.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CajaService {}
+export class CajaService {
+    constructor(
+        private cajaRepository: Repository<Caja>,
+    ) {}
+
+    async getAllCajas(limit: number) {
+        return this.cajaRepository.find({take: limit});
+    }
+}
