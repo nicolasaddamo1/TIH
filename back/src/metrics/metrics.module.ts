cc727366ccb840dcc774e0848c4ec193a151b835
Nico <nicolasaddamo1@gmail.com>
2024-11-25 16:22:19 -0300

    now yes

--- back/src/metrics/metrics.module.ts
+++ back/src/metrics/metrics.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MetricsController } from './metrics.controller';
+import { MetricsService } from './metrics.service';
 
-@Module({})
+@Module({
+  controllers: [MetricsController],
+  providers: [MetricsService]
+})
 export class MetricsModule {}
