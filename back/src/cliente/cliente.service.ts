1540b51ef1e3a2cc3c20b54310e5590002b7ed41
nicolasaddamo1 <nicolasaddamo1@gmail.com>
2024-12-14 00:51:11 -0300

    ready client

--- back/src/cliente/cliente.service.ts
+++ back/src/cliente/cliente.service.ts
@@ -11,7 +11,7 @@ export class ClienteService{
         @InjectRepository(Cliente) private clienteRepository: Repository<Cliente>
     )
     {}
-
+    
     async getAllClients(){
         return this.clienteRepository.find()
     }
@@ -20,19 +20,27 @@ export class ClienteService{
         const cliente = await this.clienteRepository.findOneBy({dni})
         return cliente.id
     }
-
+    
     async updateClients(id:string, data){
         const foundedClient = await this.clienteRepository.findOneBy({id})
         if (!foundedClient){
             return "Cliente No encontrado"
         }
-
+        
         foundedClient.apellido=data.apellido
         foundedClient.direccion=data.direccion
         foundedClient.dni=data.dni
         foundedClient.nombre=data.nombre
         foundedClient.nroTelefono=data.nroTelefono
-
+        
         return await this.clienteRepository.update(id, foundedClient)
+    }
+    async deleteClient(id: string) {
+        const foundedClient = await this.clienteRepository.findOneBy({id})
+        if (!foundedClient){
+            return "Cliente No encontrado"
+        }
+        
+        return this.clienteRepository.delete(id)
     }
 }
